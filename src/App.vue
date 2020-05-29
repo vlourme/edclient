e33fb431b3860c48f209c476359593258560bf44
Victor Lourme <victor.lourme@icloud.com>
2020-05-29 23:19:30 +0200

    views/app: update sidebar
    Signed-off-by: Victor Lourme <victor.lourme@icloud.com>

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,7 @@
             <v-icon>mdi-view-dashboard</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Dashboard</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-cog</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Settings</v-list-item-title>
+            <v-list-item-title>Panneau de bord</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
