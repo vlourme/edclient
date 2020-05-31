4d82703c29d5b2a874cd4d4fc1bb99fbdf89cf0e
Victor Lourme <victor.lourme@icloud.com>
2020-05-31 21:28:40 +0200

    views/Dashboard: add data from messages
    Signed-off-by: Victor Lourme <victor.lourme@icloud.com>

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,27 +1,75 @@
 <template>
   <div class="dashboard">
     <v-container fluid>
-      <h1 class="display-2">Compte</h1>
+      <h1 class="display-1">Compte</h1>
       <br />
       <v-card elevation="3">
         <v-card-text>
-          Email (compte): {{ profile.email }}
+          Email du compte: {{ profile.email }}
           <br />
-          Classe: {{ profile.classeLibelle }} &mdash; <b>{{ profile.classeCode }}</b>
+          Email perso lié à la messagerie: {{ settings.mailPerso }}
+          <br />
+          <div v-if="settings.mailPro">
+            Email pro lié à la messagerie: {{ settings.mailPro }}
+            <br />
+          </div>
+          Classe: {{ profile.classeLibelle }} &mdash;
+          <b>{{ profile.classeCode }}</b>
           <br />
           Type de compte: {{ profile.typeCompte === 'E' ? "Elève" : profile.typeCompte }}
         </v-card-text>
+      </v-card>
+      <br />
+      <h1 class="display-1">Messages</h1>
+      <br />
+      <v-card elevation="3">
+        <v-data-table sort-by="date" sort-desc :headers="messagesHeader" :items="messagesList"></v-data-table>
       </v-card>
     </v-container>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapActions } from "vuex";
 
 export default {
+  data: () => {
+    return {
+      messagesHeader: [
+        { 
+          text: 'Expéditeur',
+          value: 'from'
+        },
+        {
+          text: 'Sujet',
+          value: 'subject'
+        },
+        {
+          text: 'Date',
+          value: 'date'
+        }
+      ],
+      messagesList: []
+    };
+  },
   computed: {
-    ...mapGetters(["profile", "school"])
+    ...mapGetters(["profile", "school", "messages", "settings"])
+  },
+  methods: {
+    ...mapActions(["fetchMessages"])
+  },
+  beforeMount() {
+    // Get messages
+    this.fetchMessages();
+
+    // Set messages
+    this.messages.received.forEach(message => {
+      this.messagesList.push({
+        'from': message.from.name,
+        'subject': message.subject,
+        'date': message.date
+      })
+    })
   }
 };
 </script>
